2b4d0021226d353baf7e2891ff6a0c9767795cac
costalauro <costalauro@hotmail.com>
2021-04-07 20:53:03 -0300

    adicao de algumas condicoes

--- adivinhacao.py
+++ adivinhacao.py
@@ -3,14 +3,27 @@ print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 
 numero_secreto = 42
+total_de_tentativas = 3
+rodada = 1
 
-chute_str = input("Digite o seu numero: ")
+while(rodada <= total_de_tentativas):
+    print("Tentativa: ", rodada, " de ", total_de_tentativas)
+    chute_str = input("Digite o seu numero: ")
+    print("Você digitou ", chute_str)
+    chute = int(chute_str)
 
-print("Você digitou ", chute_str)
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
 
-chute = int(chute_str)
+    if acertou:
+        print("Você acertou")
+    else:
+        if maior:
+            print("Você errou! O seu chute foi maior do que o número secreto.")
+        elif menor:
+            print("Você errou! O seu chute foi menor do que o número secreto.")
 
-if(numero_secreto == chute):
-    print("Você acertou")
-else:
-    print("Você errou")
+    rodada = rodada + 1
+
+print("Fim do jogo")
